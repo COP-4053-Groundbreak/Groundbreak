e855c14a7543ba0d28ca0b33f29ac68198b6ea60
paul-rl <35239075+paul-rl@users.noreply.github.com>
2022-03-05 20:42:58 -0500

    Added main menu & optimized tile element UI

--- Groundbreak/Assets/Scripts/Tile/TileUI/ElementDisplayUI.cs
+++ Groundbreak/Assets/Scripts/Tile/TileUI/ElementDisplayUI.cs
@@ -13,4 +13,19 @@ public class ElementDisplayUI : MonoBehaviour
             Instantiate(elemVisual, child.gameObject.transform.position, Quaternion.identity, child);
         }
     }  
+
+    // Currently called all the time and only shown when player presses space.
+    // Should probably optimize but works for now
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            foreach (Transform child in tileHolder.transform){
+                Debug.Log("Looking through children");
+                if (child.childCount > 0){
+                    GameObject elemVisual = child.GetChild(0).gameObject;
+                    elemVisual.SetActive(!elemVisual.gameObject.activeInHierarchy);
+                    Debug.Log($"After the change, the GO is now {elemVisual.gameObject.activeInHierarchy}");
+                }
+            }
+        }
+    }
 }
--- Groundbreak/Assets/Scripts/Tile/TileUI/elemVisual.cs
+++ Groundbreak/Assets/Scripts/Tile/TileUI/elemVisual.cs
@@ -6,7 +6,6 @@ public class elemVisual : MonoBehaviour
 {
     // Elements stored in alphabetical ordered, accessible by bitshifting
     [SerializeField] Sprite[] elementSymbols;
-    private bool isVisible = false;
     // Index in array of corresponding visual
     int idx;
     // Start is called before the first frame update
@@ -15,28 +14,11 @@ public class elemVisual : MonoBehaviour
         // Element to index convertor
         idx = ReactionManager.elementToIdx(transform.parent.GetComponent<Tile>().getElement());
         setSymbol();
-        setVisibility(false);
-    }
-
-    // Currently called all the time and only shown when player presses space.
-    // Should probably optimize but works for now
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            setVisibility(!isVisible); // Invert visibility
-        }
+        this.gameObject.SetActive(false);
     }
 
     // Sets whether the UI is visible or not. GameObject ALWAYS exists and is active,
     // not sure if this is what's best for performance
-    private void setVisibility(bool visible){
-        if (visible){
-            this.gameObject.transform.localScale = new Vector3(1, 1, 1);
-            isVisible = true;
-        } else {
-            this.gameObject.transform.localScale = new Vector3(0, 0, 0);
-            isVisible = false;
-        }
-    }
 
     // Sets symbol of UI to whatever element its parent tile is
     public void setSymbol(){
