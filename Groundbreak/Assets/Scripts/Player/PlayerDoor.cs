8ed5318b8366874ac5db2e401d7f5ee65c25611e
MrRadir <83057195+MrRadir@users.noreply.github.com>
2022-03-09 23:28:50 -0500

    Got rooms activating and deactivating when you walk through doors

--- Groundbreak/Assets/Scripts/Player/PlayerDoor.cs
+++ Groundbreak/Assets/Scripts/Player/PlayerDoor.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class PlayerDoor : MonoBehaviour
 {
     private GameObject currentTeleporter;
+    public GameObject currentRoom;
 
     void Update()
     {
         if(currentTeleporter != null)
         {
             transform.position = currentTeleporter.GetComponent<Door>().GetDestination().position;
+
+            foreach (Transform child in currentRoom.transform)
+            {
+                Debug.Log("Inside foreach");
+                if ((child.name == "PlayerDetector1" || child.name == "PlayerDetector2" || child.name == "PlayerDetector3" || child.name == "PlayerDetector4"))
+                {
+                    Debug.Log("set a detector as true");
+                    child.gameObject.SetActive(true);
+                }
+                else
+                {
+                    Debug.Log("set an object as false");
+                    child.gameObject.SetActive(false);
+                }
+            }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Door"))
+        Debug.Log("Collision with door! This happened");
+
+        if (collision.CompareTag("Door"))
         {
-            Debug.Log("Collision with door! This happened");
             currentTeleporter = collision.gameObject;
         }
-
-        //Make it deactive current room
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -34,7 +49,5 @@ public class PlayerDoor : MonoBehaviour
                 currentTeleporter = null;
             }
         }
-
-        //Make it active new room?
     }
 }
--- Groundbreak/Assets/Scripts/RandomGenScripts/PlayerDetector.cs
+++ Groundbreak/Assets/Scripts/RandomGenScripts/PlayerDetector.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayerDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject currentRoom;
+    [SerializeField] public PlayerDoor PlayerRoom;
 
     // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        PlayerRoom = FindObjectOfType<PlayerDoor>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision with player has happened");
+
+        //PlayerRoom.currentRoom = currentRoom;
+
+        foreach (Transform child in currentRoom.transform)
+        {
+            Debug.Log("Inside foreach");
+            if ( (child.name == "PlayerDetector1" || child.name == "PlayerDetector2" || child.name == "PlayerDetector3" || child.name == "PlayerDetector4"))
+            {
+                Debug.Log("set a detector as false");
+                child.gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("set an object as true");
+                child.gameObject.SetActive(true);
+            }
+        }
+
+        PlayerRoom.currentRoom = currentRoom;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Player has left the box thing");
+        /*foreach (Transform child in currentRoom.transform)
+        {
+            Debug.Log("Inside foreach");
+            if ((child.name == "PlayerDetector1" || child.name == "PlayerDetector2" || child.name == "PlayerDetector3" || child.name == "PlayerDetector4"))
+            {
+                Debug.Log("set a detector as false");
+                child.gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("set an object as true");
+                child.gameObject.SetActive(true);
+            }
+        }*/
     }
 }
