e329e028418b1faefa344ad197c90ecc25068ee9
MrRadir <83057195+MrRadir@users.noreply.github.com>
2022-03-10 19:16:17 -0500

    Saved ChestSpawner

--- Groundbreak/Assets/Scripts/RandomGenScripts/ChestSpawner.cs
+++ Groundbreak/Assets/Scripts/RandomGenScripts/ChestSpawner.cs
@@ -7,6 +7,7 @@ public class ChestSpawner : MonoBehaviour
     [SerializeField] bool Spawned = false;
     [SerializeField] int RoomWidth;
     [SerializeField] int RoomHeight;
+    [SerializeField] GameObject Chest;
 
     private int randX;
     private int randY;
@@ -16,9 +17,15 @@ public class ChestSpawner : MonoBehaviour
     {
         randX = Random.Range(0, RoomWidth);
         randY = Random.Range(0, RoomHeight);
+        Vector3 posChest = new Vector3(randX, randY, 0);
+
+        Chest.transform.localPosition = 
+
 
         //Instantiate a chest
-        //Instantiate
+        Instantiate(Chest, Chest.transform.position, Chest.transform.rotation);
+        Chest.transform.localPosition = posChest;
+        Spawned = true;
     }
 
     
