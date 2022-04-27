731fd40ef263fa814bbca0b277ede63fddbb3195
paul-rl <35239075+paul-rl@users.noreply.github.com>
2022-04-27 14:51:40 -0400

    Made seed random if nothing input

--- Groundbreak/Assets/Scripts/RandomGenScripts/Seed.cs
+++ Groundbreak/Assets/Scripts/RandomGenScripts/Seed.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Seed : MonoBehaviour
 {
     public string gameSeed = "Default";
+    public bool playerInput = false;
     public int currentSeed = 0;
 
 
     public void Awake()
     {
+        const string chars = "ABCDEFGHIJKLMNOPRSTUVWXYZ0123456789";
+        if (!playerInput){
+            gameSeed = new string(Enumerable.Repeat(chars,8).Select(s => s[(int)(Random.value * s.Length)]).ToArray());
+        }
         currentSeed = gameSeed.GetHashCode();
         Random.InitState(currentSeed);
     }
