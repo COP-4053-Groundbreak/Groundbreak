18d3601fa8b3a5271ccf74688139ba67f13e0b94
paul-rl <35239075+paul-rl@users.noreply.github.com>
2022-04-27 14:58:39 -0400

    Added tutorial to main menu

--- Groundbreak/Assets/Scripts/MainMenu/MainMenu.cs
+++ Groundbreak/Assets/Scripts/MainMenu/MainMenu.cs
@@ -8,4 +8,8 @@ public class MainMenu : MonoBehaviour
     public void PlayGame(){
         SceneManager.LoadSceneAsync("Level 1");
     }
+
+    public void PlayTutorial(){
+        SceneManager.LoadSceneAsync("Tutorial");
+    }
 }
