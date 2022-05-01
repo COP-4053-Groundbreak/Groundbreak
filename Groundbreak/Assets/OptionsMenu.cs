fd8d0573e60049e1326ddcb41f7d8464b180170b
paul-rl <35239075+paul-rl@users.noreply.github.com>
2022-05-01 15:58:19 -0400

    Fixed - volume

--- Groundbreak/Assets/OptionsMenu.cs
+++ Groundbreak/Assets/OptionsMenu.cs
@@ -21,13 +21,17 @@ public class OptionsMenu : MonoBehaviour
         audioMixer.GetFloat("MasterVolume", out currVol);
         currVol = Mathf.Pow(10, currVol / 20);
         
-        
+
         volumePercent.GetComponent<TextMeshProUGUI>().SetText((int)(currVol * 100) + "%");   
         volumePercent.transform.parent.Find("Volume Slider").GetComponent<Slider>().SetValueWithoutNotify(currVol);
     }
     public void setVolume(float rawVolume){  
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(rawVolume) * 20);
         volumePercent.GetComponent<TextMeshProUGUI>().SetText(((int)(rawVolume * 100)).ToString() + "%");
+    }
+
+    public void setFullscreen(bool isFullscreen){
+        Screen.fullScreen = isFullscreen;
     }
 
     public void BackToMenu(){
