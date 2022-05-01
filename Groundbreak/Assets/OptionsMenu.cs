f374b4383067f2b61a300d566511330dd50248ae
paul-rl <35239075+paul-rl@users.noreply.github.com>
2022-05-01 17:32:42 -0400

    Fullscreen toggle is done

--- Groundbreak/Assets/OptionsMenu.cs
+++ Groundbreak/Assets/OptionsMenu.cs
@@ -9,13 +9,15 @@ public class OptionsMenu : MonoBehaviour
 {
     GameObject mainMenu;
     GameObject volumePercent;
-
+    GameObject fullscreenToggle;
+     GameObject xIndic;
     public AudioMixer audioMixer;
     
     private void Start() {
         mainMenu = this.transform.parent.Find("MainMenu").gameObject;        
         volumePercent = this.transform.Find("Volume").transform.Find("VolumePercent").gameObject;
-        
+        fullscreenToggle = this.transform.Find("Fullscreen").transform.Find("FullscreenToggle").gameObject;
+        xIndic = fullscreenToggle.transform.Find("X").gameObject;
         // Set % to value sound is currently at
         float currVol;
         audioMixer.GetFloat("MasterVolume", out currVol);
@@ -23,6 +25,14 @@ public class OptionsMenu : MonoBehaviour
 
         volumePercent.GetComponent<TextMeshProUGUI>().SetText((int)(currVol * 100) + "%");   
         volumePercent.transform.parent.Find("Volume Slider").GetComponent<Slider>().SetValueWithoutNotify(currVol);
+        // Set X on FullScreen
+        if (Screen.fullScreen){
+            xIndic.GetComponent<TextMeshProUGUI>().SetText("X");
+            fullscreenToggle.GetComponent<Toggle>().isOn = true;
+        } else {
+            xIndic.GetComponent<TextMeshProUGUI>().SetText("");
+            fullscreenToggle.GetComponent<Toggle>().isOn = false;
+        }
     }
     public void setVolume(float rawVolume){  
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(rawVolume) * 20);
@@ -32,6 +42,12 @@ public class OptionsMenu : MonoBehaviour
     public void setFullscreen(bool isFullscreen){
         Screen.fullScreen = isFullscreen;
         Debug.Log("Set to " + isFullscreen);
+       
+        if (fullscreenToggle.GetComponent<Toggle>().isOn){
+            xIndic.GetComponent<TextMeshProUGUI>().SetText("X");
+        } else {
+            xIndic.GetComponent<TextMeshProUGUI>().SetText("");
+        }
     }
 
     public void BackToMenu(){
